494f463823003de02fe7b36a9d68136f01a3a525
Greeshma <lgreeshma783@gmail.com>
2026-02-07 19:44:59 +0530

    project structure

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+export const Footer = () => {
+    return (
+        <footer>
+            
+
+<footer className="bg-neutral-primary-soft shadow-xs border border-default m-1">
+    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span className="text-sm text-body sm:text-center">© 2026 <Link to="/" className="hover:underline">Cinemate™</Link>. All Rights Reserved.
+    </span>
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
+        <li>
+            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+        </li>
+        <li>
+            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+        </li>
+        <li>
+            <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+        </li>
+        <li>
+            <a href="#" target="_blank" className="hover:underline">Github</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
+        </footer>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
